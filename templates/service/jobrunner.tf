c99d18a6a7975ba0ad30e8d11840f3216294ff6a
gshamann <gshamann@in.ibm.com>
2022-06-29 18:02:31 +0530

    Add agent location

--- templates/service/jobrunner.tf
+++ templates/service/jobrunner.tf
@@ -20,8 +20,9 @@ resource "kubernetes_config_map" "jobrunner_configmap" {
   }
 
   data = {
-    JR_PROFILEID         = var.profile_id
-    JR_AGENTNAME         = var.agent_name
+    JR_PROFILEID          = var.profile_id
+    JR_AGENTNAME          = var.agent_name
+    JR_AGENT_LOCATION     = var.location
     JR_SCHEMATICSENDPOINT = local.schematics_endpoint
     JR_EXTLOGPATH         = "/var/log/schematics/%s.log"
     JR_SAVESERVICECOPY    = true
